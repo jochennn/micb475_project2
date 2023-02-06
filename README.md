1d16635b470b3d5692a406771e1b7c0f567078ac
Jordon Chen <jordonochen@gmail.com>
2023-02-05 20:45:34 -0800

    test all commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # micb475_project2
 repository for project 2 micb475
+A line I wrote on my local computer
--- hello_test.R
+++ hello_test.R
@@ -1,1 +1,5 @@
 print("hello group members")
+group_10 <- data.frame(name=c("jordon","chris","fione","vivian"),
+                       age=c(901,29,219,8),
+                       github=c(FALSE,FALSE,FALSE,FALSE))
+head(group_10)
