f100d9777143dd62687ec50b11db659b36f9311e
Chris Yap <seewhyeleven@hotmail.ca>
2023-03-21 13:42:54 -0700

    created rough script for "core_microbiome"

--- core_microbiome.R
+++ core_microbiome.R
@@ -1,24 +1,53 @@
+# load packages
 library(phyloseq)
 library(microbiome)
-
-
-phyloseq_object <- 
-  transform_sample_counts(phyloseq_rawabundance, fun=function(x) x/sum(x))
-
-
-group1_phyloseq <- subset_samples(phyloseq_object, Group=="group1")
-group2_phyloseq <- subset_samples(phyloseq_object, Group=="group2")
-
-
-group1_core <- core_members(group1_phyloseq, detection=0, prevalence=0.8)
-group2_core <- core_members(group2_phyloseq, detection=0, prevalence=0.8)
-
 library(ggVennDiagram)
-ggVennDiagram(x=list(group1_core, group2_core))
 
+# Convert to relative abundance
+fish_phyloseq_RA <- 
+  transform_sample_counts(fish_phyloseq, fun=function(x) x/sum(x))
 
+# Filter dataset by "anat_space_combine"
+c_e_phyloseq <- 
+  subset_samples(
+    fish_phyloseq_RA, 
+    anat_space_combine=="coastal/external")
 
+c_i_phyloseq <- 
+  subset_samples(
+    fish_phyloseq_RA, 
+    anat_space_combine=="coastal/internal")
 
+o_e_phyloseq <-
+  subset_samples(
+    fish_phyloseq_RA, 
+    anat_space_combine=="open ocean/external")
+
+o_i_phyloseq <-
+  subset_samples(
+    fish_phyloseq_RA, 
+    anat_space_combine=="open ocean/internal")
+
+# What ASVs are found in more than 50% of samples in each category?
+c_e_core <- core_members(c_e_phyloseq, detection=0.001, prevalence=0.5)
+c_i_core <- core_members(c_i_phyloseq, detection=0.001, prevalence=0.5)
+o_e_core <- core_members(o_e_phyloseq, detection=0.001, prevalence=0.5)
+o_i_core <- core_members(o_i_phyloseq, detection=0.001, prevalence=0.5)
+
+# venn diagram
+ggVennDiagram(x=list(c_e_core, c_i_core, o_e_core, o_i_core), 
+              category.names=list(
+                "coastal/external", 
+                "coastal/internal", 
+                "open ocean/external", 
+                "open ocean/internal"
+                ),
+              label="count")
+
+# What are these ASVs?
+prune_taxa(o_i_core,fish_phyloseq) %>%
+  plot_bar(, fill="Genus") +
+  facet_wrap(.~anat_space_combine, scales="free")
 
 
 
