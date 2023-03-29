160ab8248a16e6db21ff593e1e288c362b763b4a
Fione Yip <124647036+FifiShennel@users.noreply.github.com>
2023-03-28 23:10:24 -0700

    Update weighted_unifrac.R
    Updated file

--- weighted_unifrac.R
+++ weighted_unifrac.R
@@ -9,3 +9,10 @@ load("./create_phyloseq_obj/outputs/fish_phyloseq.Rdata")
 
 # make a distance matrix 
 wunifrac_dm <- distance(fish_phyloseq, method="wunifrac")
+pcoa_wu <- ordinate(fish_phyloseq, method="PCoA", distance = wunifrac_dm)
+
+plot_ordination(fish_phyloseq, pcoa_wu, color = "elevation", shape = "transectname") + 
+  scale_color_gradient(low="darkblue", high ="lightblue") + 
+  labs(pch = "Transect name", col ="Elevation")
+
+  
