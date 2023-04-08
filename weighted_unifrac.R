364f5bf8692243a6a65bb667b7a2c716b0ff95f5
Fione Yip <124647036+FifiShennel@users.noreply.github.com>
2023-04-07 22:35:13 -0700

    Update

--- weighted_unifrac.R
+++ weighted_unifrac.R
@@ -11,8 +11,25 @@ load("./create_phyloseq_obj/outputs/fish_phyloseq.Rdata")
 wunifrac_dm <- distance(fish_phyloseq, method="wunifrac")
 pcoa_wu <- ordinate(fish_phyloseq, method="PCoA", distance = wunifrac_dm)
 
-plot_ordination(fish_phyloseq, pcoa_wu, color = "elevation", shape = "transectname") + 
-  scale_color_gradient(low="darkblue", high ="lightblue") + 
-  labs(pch = "Transect name", col ="Elevation")
+plot_ordination(fish_phyloseq, pcoa_wu, color = "anatomical_location", shape = "spatial_region") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Weighted Unifrac") 
+
+plot_ordination(fish_phyloseq, pcoa_wu, color = "sample_type", shape = "habitat_depth_level3") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Weighted Unifrac") 
+
+plot_ordination(fish_phyloseq, pcoa_wu, color = "anat_space_combine") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Weighted Unifrac") 
+
+plot_ordination(fish_phyloseq, pcoa_wu, color = "sample_type") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Weighted Unifrac") 
+
+
+
+
+  
 
   
